3d98b12cb3d47d613c1d8a7bddf1c1102972e358
Marcosdbras <marcosbras@hotmail.com>
2018-09-24 16:53:13 -0300

    Update database.php

--- bar/app/config/database.php
+++ bar/app/config/database.php
@@ -13,9 +13,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'bar';
+$db['default']['username'] = 'admin';
+$db['default']['password'] = 'bios2805';
+$db['default']['database'] = 'lanchonete_bar';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
